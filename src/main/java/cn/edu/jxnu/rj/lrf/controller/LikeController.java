fb46fc74a380aee4af5185dbee98bb8f4bf221e0
205331411 <75103898+205331411@users.noreply.github.com>
2021-05-18 08:30:59 +0800

    点赞路径

--- src/main/java/cn/edu/jxnu/rj/lrf/controller/LikeController.java
+++ src/main/java/cn/edu/jxnu/rj/lrf/controller/LikeController.java
@@ -3,6 +3,8 @@ package cn.edu.jxnu.rj.lrf.controller;
 import cn.edu.jxnu.rj.lrf.common.ResponseModel;
 import cn.edu.jxnu.rj.lrf.service.LikeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,51 +21,52 @@ public class LikeController {
     @Autowired
     LikeService likeService;
 
-    @RequestMapping("/moment")
+    @PostMapping("/moment")
     public ResponseModel momentLike(int momentId,int userId){
         likeService.MomentLike(momentId,userId);
         return new ResponseModel();
     }
 
-    @RequestMapping("/cancelmoment")
-    public ResponseModel cancelmomentLike(int momentId,int userId){
-        likeService.cancelMomentLike(momentId,userId);
-        return new ResponseModel();
-    }
-
-    @RequestMapping("/ismoment")
-    public ResponseModel isMomentLike(int momentId, int userId){
-        boolean is = likeService.isMomentLike(momentId,userId);
-        return new ResponseModel(is);
-    }
-
-    @RequestMapping("/momentlikeamount")
-    public ResponseModel getMomentLikeAount(int momentId){
-        long num = likeService.getMomentLikeAmount(momentId);
-        return new ResponseModel(num);
-    }
-
-    @RequestMapping("/comment")
+    @PostMapping("/comment")
     public ResponseModel commentLike(int commentId, int userId){
         likeService.CommentLike(commentId,userId);
         return new ResponseModel();
     }
 
-    @RequestMapping("/cancelcomment")
+    @PostMapping("/cancelmoment")
+    public ResponseModel cancelmomentLike(int momentId,int userId){
+        likeService.cancelMomentLike(momentId,userId);
+        return new ResponseModel();
+    }
+
+    @PostMapping("/cancelcomment")
     public ResponseModel cancelcommentLike(int commentId,int userId){
         likeService.cancelCommentLike(commentId,userId);
         return new ResponseModel();
     }
 
-    @RequestMapping("/iscomment")
+    @GetMapping("/ismoment")
+    public ResponseModel isMomentLike(int momentId, int userId){
+        boolean is = likeService.isMomentLike(momentId,userId);
+        return new ResponseModel(is);
+    }
+
+    @GetMapping("/iscomment")
     public ResponseModel isCommentLike(int commentId,int userId){
         boolean is = likeService.isCommentLike(commentId,userId);
         return new ResponseModel(is);
     }
 
-    @RequestMapping("/commentlikeamount")
+    @GetMapping("/momentlikeamount")
+    public ResponseModel getMomentLikeAount(int momentId){
+        long num = likeService.getMomentLikeAmount(momentId);
+        return new ResponseModel(num);
+    }
+
+    @GetMapping("/commentlikeamount")
     public ResponseModel getCommentLikeAmount(int commentId){
         long num = likeService.getCommentLikeAmount(commentId);
         return new ResponseModel(num);
     }
+
 }
